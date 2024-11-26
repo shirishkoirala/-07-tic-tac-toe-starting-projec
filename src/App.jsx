f932471d97d49001b7a0ef7d21db7d3bcc352780
Shirish Koirala <shirishkoirala@gmail.com>
2024-11-26 16:13:49 +1000

    Name change logic added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
 
 function derivedActivePlayer(gameTurns) {
   let currnetPlayer = "X";
@@ -16,11 +17,17 @@ const initialGameBoard = [
   [null, null, null],
   [null, null, null]
 ]
+
 function App() {
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2"
+  })
+
   const [gameTurns, setGameTurns] = useState([]);
 
   let activePlayer = derivedActivePlayer(gameTurns)
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
   for (const turn of gameTurns) {
     const { square, player } = turn;
     const { row, col } = square;
@@ -35,9 +42,11 @@ function App() {
     const thirdSquareSymbol = gameBoard[combinations[2].row][combinations[2].column]
 
     if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && secondSquareSymbol === thirdSquareSymbol) {
-      winner = firstSquareSymbol
+      winner = players[firstSquareSymbol];
     }
   }
+  const hasDraw = gameTurns.length === 9 && !winner
+
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns(prevTurns => {
       let currnetPlayer = derivedActivePlayer(prevTurns)
@@ -46,14 +55,26 @@ function App() {
     });
   }
 
+  function handleRematch() {
+    setGameTurns([])
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      }
+    }))
+  }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
+          <Player onChangeName={handlePlayerNameChange} initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player onChangeName={handlePlayerNameChange} initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} onReseart={handleRematch} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
-
+    function handleButtonClick() {
+        setIsEditing((editing) => !editing)
+        if (isEditing) {
+            onChangeName(symbol, playerName)
+        }
+    }
     function handleChange(e) {
         setPlayerName(e.target.value)
     }
@@ -11,10 +16,10 @@ export default function Player({ initialName, symbol, isActive }) {
     return (
         <li className={isActive ? "active" : undefined}>
             <span className="player">
-                {!isEditing ? <span className="player-name">{playerName}</span> : <input type="text" value={playerName} onChange={handleChange} required/>}
+                {!isEditing ? <span className="player-name">{playerName}</span> : <input type="text" value={playerName} onChange={handleChange} required />}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={() => setIsEditing((editing) => !editing)}>{isEditing ? "Save" : "Edit"}</button>
+            <button onClick={handleButtonClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
